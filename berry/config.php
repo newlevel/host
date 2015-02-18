03a98d9b162b87ab72e2cc3e77293ee2ae71d7f7
tangopeter <peter@t1.co.nz>
2015-02-18 13:51:28 +1300

    adding subdomain sites

---
+++ berry/config.php
@@ -0,0 +1,25 @@
+<?php
+
+/* Database Connection */
+define('_DBHOST', 'localhost');
+define('_DBUSER', 'root');
+define('_DBPASS', 'keacar');
+define('_DBNAME', 'berry');
+
+/* Directory locations */
+define('_SITEURL',   'http://berry.host:8888');
+define('_BASEDIR',   '/Volumes/Seagate/Sites2/PumpCMS_4.1.1');
+define('_WEBDIR',    '/Volumes/Seagate/Sites2/host/berry');
+define('_MYSITEDIR', '/Volumes/Seagate/Sites2/domains/berry');
+
+/* Shared plugin directory (if used) */
+define('_ALTPLUGINDIR', '/Volumes/Seagate/Sites2/PumpCMS_4_plugins');
+
+/* Debug Options - not recommended for production installations */
+define('_DEBUG', false);
+
+/* Master password */
+define('_MASTERPASS', 'gofast99');
+
+/* Admin root */
+define('_ADMIN', 'admin');
---
+++ mk2/config.php
@@ -0,0 +1,25 @@
+<?php
+
+/* Database Connection */
+define('_DBHOST', 'localhost');
+define('_DBUSER', 'root');
+define('_DBPASS', 'keacar');
+define('_DBNAME', 'mk2');
+
+/* Directory locations */
+define('_SITEURL',   'http://mk2.host:8888');
+define('_BASEDIR',   '/Volumes/Seagate/Sites2/PumpCMS_4.1.1');
+define('_WEBDIR',    '/Volumes/Seagate/Sites2/host/mk2');
+define('_MYSITEDIR', '/Volumes/Seagate/Sites2/domains/mk2');
+
+/* Shared plugin directory (if used) */
+define('_ALTPLUGINDIR', '/Volumes/Seagate/Sites2/PumpCMS_4_plugins');
+
+/* Debug Options - not recommended for production installations */
+define('_DEBUG', false);
+
+/* Master password */
+define('_MASTERPASS', 'gofast99');
+
+/* Admin root */
+define('_ADMIN', 'admin');
