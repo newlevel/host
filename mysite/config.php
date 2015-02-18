2f7a1a42025d311daeda4888bcc0b8f8dc648735
tangopeter <peter@t1.co.nz>
2015-02-18 13:58:18 +1300

    adding copy of mysite

---
+++ mysite/config.php
@@ -0,0 +1,25 @@
+<?php
+
+/* Database Connection */
+define('_DBHOST', 'localhost');
+define('_DBUSER', 'root');
+define('_DBPASS', 'keacar');
+define('_DBNAME', 'tlma');
+
+/* Directory locations */
+define('_SITEURL',   'http://localhost:8888');
+define('_BASEDIR',   '/Volumes/Seagate/Sites2/PumpCMS_4.1.1');
+define('_WEBDIR',    '/Volumes/Seagate/Sites2/');
+define('_MYSITEDIR', '/Volumes/Seagate/Sites2/domains/mysite');
+
+/* Shared plugin directory (if used) */
+define('_ALTPLUGINDIR', '/Volumes/Seagate/Sites2/PumpCMS_4_plugins');
+
+/* Debug Options - not recommended for production installations */
+define('_DEBUG', true);
+
+/* Master password */
+define('_MASTERPASS', 'gofast99');
+
+/* Admin root */
+define('_ADMIN', 'admin');
